407e3859630492345d86c0979e282d4f5a8a13c2
Gabriel Debona <gabriel.debona@gmail.com>
2023-11-17 16:36:31 -0300

    feat: Created the graphql folder and added the gifs declarations

--- src/graphql/gifs.ts
+++ src/graphql/gifs.ts
@@ -1,8 +1,8 @@
 import {gql} from "urql";
 
 export const GIFS_BY_CATEGORY_QUERY = gql`
-  query gifs ($category: String, $limit: Int){
-    gifs(limit: $limit, where: {category: {_regex: $category}}) {
+  query gifs ($category: String, $limit: Int, $offset: Int){
+    gifs(limit: $limit, where: {category: {_regex: $category}}, offset: $offset) {
       url
       id
       category

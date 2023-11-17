93b7a5b135a2ed75597781c627cfc7f7978b89e5
Gabriel Debona <gabriel.debona@gmail.com>
2023-11-17 16:33:41 -0300

    feat: Created Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+type PropsInput = {
+  value: string[],
+  placeholder: string
+  onChange: (e: React.FormEvent<HTMLInputElement>) => void
+}
+
+export const Input: React.FC = (props: PropsInput) => {
+  return (
+    <input
+      value={props.value}
+      onChange={props.onChange}
+      placeholder={props.placeholder}
+      className="input"
+    />
+  );
+};

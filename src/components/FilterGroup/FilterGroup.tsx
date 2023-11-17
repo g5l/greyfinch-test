7c5a9056e195249dc28ee40699334a4eaf414678
Gabriel Debona <gabriel.debona@gmail.com>
2023-11-17 16:32:57 -0300

    feat: Created FilterGroup component

---
+++ src/components/FilterGroup/FilterGroup.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+type PropsFilterGroup = {
+  values: string[],
+  onClick: (e: React.MouseEvent<HTMLElement>) => void
+}
+
+export const FilterGroup: React.FC = (props: PropsFilterGroup) => {
+  return (
+    <div className="filterGroup-container">
+      {props.values.map((value, index) => (
+        <button key={index} value={value} onClick={props.onClick} className="filterGroup-item">
+          {value}
+        </button>
+      ))}
+    </div>
+  );
+};
